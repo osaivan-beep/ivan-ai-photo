adca088be7f7ca7973a0c31b7017650e9cf8d73b
ivan-ai <osa.ivan@gmail.com>
2025-12-01 19:56:37 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'ivan-ai-photo-cache-v40010'; // Updated to v40010 to force refresh
+const CACHE_NAME = 'ivan-ai-photo-cache-v40013'; // Updated to v40013 to force refresh
 const APP_SHELL_URLS = [
   '/',
   '/index.html',
